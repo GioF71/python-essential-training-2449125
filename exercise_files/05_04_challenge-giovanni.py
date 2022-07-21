5335bd46caf4bd123bdcae861e97ef0b30cd3011
GioF_71 <giovanni.fulco@gmail.com>
2022-07-21 19:44:18 +0200

    Challenge 05

--- exercise_files/05_04_challenge-giovanni.py
+++ exercise_files/05_04_challenge-giovanni.py
@@ -10,17 +10,8 @@ class Canvas:
         self._y = height
         self._canvas = [[' ' for y in range(self._y)] for x in range(self._x)]
 
-    def hitsVerticalWall(self, point):
-        return round(point[0]) < 0 or round(point[0]) >= self._x
-
-    def hitsHorizontalWall(self, point):
-        return round(point[1]) < 0 or round(point[1]) >= self._y
-
     def hitsWall(self, point):
-        return self.hitsVerticalWall(point) or self.hitsHorizontalWall(point)
-
-    def getReflection(self, point):
-        return [-1 if self.hitsVerticalWall(point) else 1, -1 if self.hitsHorizontalWall(point) else 1]
+        return round(point[0]) < 0 or round(point[0]) >= self._x or round(point[1]) < 0 or round(point[1]) >= self._y
 
     def setPos(self, pos, mark):
         self._canvas[round(pos[0])][round(pos[1])] = mark
@@ -49,36 +40,23 @@ class TerminalScribe:
         radians = (degrees/180) * math.pi
         return [math.sin(radians), -math.cos(radians)]
 
-    def setDegrees(self, degrees):
-        radians = (degrees/180) * math.pi
-        self.direction = self.calcDirection(degrees)
-
     def up(self):
-        self.forward(0, 1)
+        self.forward(0)
 
     def down(self):
-        self.forward(180, 1)
+        self.forward(180)
 
     def right(self):
-        self.forward(90, 1)
+        self.forward(90)
 
     def left(self):
-        self.forward(270, 1)
+        self.forward(270)
 
-    def bounce(self, pos, direction):
-        reflection = self.canvas.getReflection(pos)
-        return [direction[0] * reflection[0],
-                direction[1] * reflection[1]]
-
-    def forward(self, degrees, distance):
+    def forward(self, degrees):
         direction = self.calcDirection(degrees)
-        for i in range(distance):
-            pos = [self.pos[0] + direction[0],
-                   self.pos[1] + direction[1]]
-            if self.canvas.hitsWall(pos):
-                direction = self.bounce(pos, direction)
-                pos = [self.pos[0] + direction[0],
-                       self.pos[1] + direction[1]]
+        pos = [self.pos[0] + direction[0],
+               self.pos[1] + direction[1]]
+        if not self.canvas.hitsWall(pos):
             self.draw(pos)
 
     def drawSquare(self, size):
@@ -107,12 +85,9 @@ scribe.setPosition([0, 0])
 scribe.drawSquare(10)
 
 # restore initial position
+# setPosition method should not necessarily be available also on the challenge
+# I created it so that I can also draw the rectangle
 scribe.setPosition([0, 0])
 
-# challenge solution hereafter
-# my changes:
-#   there is no setDegrees method: degrees are the first argument for the method forward
-#   second argument for forward is distance
-#   also degrees is passed to calcDirection and to bounce
-#   bounce now calculates and returns the new direction
-scribe.forward(150, 100)
+for i in range(30):
+    scribe.forward(135)
